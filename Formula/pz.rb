f8d8a9bd88bd2786dbe4ac4f50b1b0b1013d6f68
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-03 16:02:12 +0900

    Brew formula update for pz-launcher version 0.1.3

--- Formula/pz.rb
+++ Formula/pz.rb
@@ -5,21 +5,21 @@
 class Pz < Formula
   desc "A project-zer0/pz launcher"
   homepage "https://github.com/project-zer0/pz-launcher"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/project-zer0/pz-launcher/releases/download/0.1.2/pz-launcher_0.1.2_Darwin_x86_64.tar.gz"
-    sha256 "cf4cd423109ccd10f75dc2ffce587bb1f9822340aeab2e49256a3c8236fbdd37"
+    url "https://github.com/project-zer0/pz-launcher/releases/download/0.1.3/pz-launcher_0.1.3_Darwin_x86_64.tar.gz"
+    sha256 "ec2168d2b236d0d556aa4e23ffecfa65cd1170d32aabcc1bf35556f4e442cad6"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/project-zer0/pz-launcher/releases/download/0.1.2/pz-launcher_0.1.2_Linux_x86_64.tar.gz"
-    sha256 "f7bb65bdc48637f4859159364e8dc0e6b4efd0a64341a909ff6bd985d26dd607"
+    url "https://github.com/project-zer0/pz-launcher/releases/download/0.1.3/pz-launcher_0.1.3_Linux_x86_64.tar.gz"
+    sha256 "aae6d4502286fdab0b40d8d2474e298aaa454fccb698ead3efaf84d9446bfda3"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/project-zer0/pz-launcher/releases/download/0.1.2/pz-launcher_0.1.2_Linux_arm64.tar.gz"
-    sha256 "6797deac2a2382444ffa6bb1c659de977b07eba3d81c1918798d59961dff9ec1"
+    url "https://github.com/project-zer0/pz-launcher/releases/download/0.1.3/pz-launcher_0.1.3_Linux_arm64.tar.gz"
+    sha256 "7714b16971026e9c466ac58beef9aea76a44e182b4e7176bb07d118f13f846b7"
   end
 
   head "https://github.com/project-zer0/pz-launcher.git"
